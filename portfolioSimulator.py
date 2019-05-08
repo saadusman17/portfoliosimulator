64a4920ce558ff14df8cd44313097ea6675bf1be
Saad <saadusman17@gmail.com>
2019-05-08 20:32:25 +1000

    Added dividend payments

--- portfolioSimulator.py
+++ portfolioSimulator.py
@@ -1,13 +1,17 @@
 import datetime
 import pprint
+import datetime
+import random
+import matplotlib.pyplot as plt
+
 pp = pprint.PrettyPrinter(indent=4)
 
 shares = [{"ShareName":"VAS", "Price":10, "DividendYield": 2, "FrankingLevel": 70, "DividendsPerYear":4, "Allocation":50}, 
 {"ShareName":"AFI", "Price":25, "DividendYield":4, "FrankingLevel":100, "DividendsPerYear":4, "Allocation":50}]
 
-portfolioTarget = 100000
-MonthlyInvestment = 1000
-growthRate = 6
+portfolioTarget = 1000000
+MonthlyInvestment = 3000
+growthRate = 6 #Including dividends
 
 portfolioLots = []
 portfolioSummary = {}
@@ -18,8 +22,13 @@ date = datetime.datetime.now()
 for share in shares:
     portfolioSummary[share["ShareName"]] = 0
     
+iteration_counter = 0
+
+dates = [] #for plotting
+portfolioValues = [] #for plotting
 
 while portfolio < portfolioTarget:
+    
     #Each iteration of this loop will add one month's investments
     sum_of_allocations = 0 
     for share in shares:
@@ -35,25 +44,56 @@ while portfolio < portfolioTarget:
         portfolioSummary[share["ShareName"]] += position
     assert(sum_of_allocations == 100)
 
-    #Month has now passed, calculating updated values
-    date += datetime.timedelta(days=35)
-    date = datetime.datetime(date.year, date.month, 1)
+    #Dividend payment and reinvestment
+    for share in shares:
+        if(date.month % (12/ share["DividendsPerYear"]) == 0): #Will work for quarterly or semiannually dividends, might need to fix for other cases
+            print("Yay!! DividendTime")
+            dividend_value_per_share = share["Price"]*share["DividendYield"]/float(share["DividendsPerYear"])/100
+            reinvestment_price = share["Price"] - dividend_value_per_share
+            shares_repurchased = dividend_value_per_share * portfolioSummary[share["ShareName"]] / reinvestment_price
+            portfolioLots.append({"Date": date, "ShareName": share["ShareName"], "BuyingPrice": reinvestment_price, "Position": shares_repurchased})
+            share["Price"] = reinvestment_price
+            portfolioSummary[share["ShareName"]] += shares_repurchased
 
     portfolio = 0 #Resetting portfolio to recalculate its value based on holdings
     for share in shares:
         share["Price"] += (share["Price"] * growthRate/float(12)/100)
         portfolio += share["Price"] * portfolioSummary[share["ShareName"]]
     print(date, portfolio)
+    dates.append(date)
+    portfolioValues.append(portfolio)
 
-    #Need to pay dividends and repurchase that stuff
+    #Adding date and portfolio to arrays to be plotted later
+
+    #Month has now passed, updating dates
+    date += datetime.timedelta(days=35)
+    date = datetime.datetime(date.year, date.month, 1)
+
+    iteration_counter += 1
+
+# plot
+plt.plot(dates,portfolioValues)
+# beautify the x-labels
+plt.gcf().autofmt_xdate()
+
+plt.show()
+
+# exit()
+# pp.pprint("Lots:")    
+# pp.pprint(portfolioLots)
+# pp.pprint("PortfolioValue:")    
+# pp.pprint(portfolio)
+# pp.pprint("PortfolioSummary:")    
+# pp.pprint(portfolioSummary)
+# pp.pprint("Shares Situation:")    
+# pp.pprint(shares)
 
 #Need to redraw money from portfolio
 
-pp.pprint("Lots:")    
-pp.pprint(portfolioLots)
-pp.pprint("PortfolioValue:")    
-pp.pprint(portfolio)
-pp.pprint("PortfolioSummary:")    
-pp.pprint(portfolioSummary)
-pp.pprint("Shares Situation:")    
-pp.pprint(shares)
+yearly_withdrawal_amount = 60000
+monthly_withdrawal_amount = yearly_withdrawal_amount/float(12)
+withdrawal_months = 12*100 #100 years
+withdrawal_percent = 4
+
+for i in range(0, withdrawal_months):
+    pass #to be done
